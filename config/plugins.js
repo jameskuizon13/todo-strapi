b2d3b7ffb3f7ed2ce035fd3065db1345b158e3b5
James Michael Kuizon <jameskuizon13@gmail.com>
2023-04-24 18:08:24 +0800

    Updated config

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,4 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   upload: {
     config: {
       provider: "aws-s3",
@@ -8,6 +8,15 @@ export default ({ env }) => ({
         region: env("AWS_REGION"),
         params: {
           Bucket: env("AWS_BUCKET_NAME"),
+        },
+      },
+      // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
+      actionOptions: {
+        upload: {
+          ACL: null,
+        },
+        uploadStream: {
+          ACL: null,
         },
       },
     },
